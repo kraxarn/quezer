c7ee212c31fb8095ce2c054f07d3c19b8da50b22
kraxarn <kraxie@protonmail.com>
2026-03-18 16:38:55 +0100

    Add song/album/artist info to song data

--- src/deezer/objects/songdata.cpp
+++ src/deezer/objects/songdata.cpp
@@ -1,18 +1,59 @@
 #include "deezer/objects/songdata.hpp"
 
+#include <QJsonArray>
+
 auto SongData::fromJson(const QJsonObject &json) -> SongData
 {
-	SongData result;
+	SongData songData;
 
 	const QJsonObject results = json
 		.value(QStringLiteral("results")).toObject();
 
-	result.mTrackToken = results.value(QStringLiteral("TRACK_TOKEN")).toString();
-	result.mTrackTokenExpire = QDateTime::fromSecsSinceEpoch(
+	const QJsonArray artists = results
+		.value(QStringLiteral("ARTISTS")).toArray();
+
+	songData.mAlbId = results.value(QStringLiteral("ALB_ID")).toString().toLongLong();
+	songData.mAlbPicture = results.value(QStringLiteral("ALB_PICTURE")).toString();
+	songData.mAlbTitle = results.value(QStringLiteral("ALB_TITLE")).toString();
+
+	songData.mArtists.reserve(artists.size());
+	for (const QJsonValueConstRef artist: artists)
+	{
+		songData.mArtists.append(Artist::fromJson(artist.toObject()));
+	}
+
+	songData.mDigitalReleaseDate = QDate::fromString(
+		results.value(QStringLiteral("DIGITAL_RELEASE_DATE")).toString(),
+		Qt::ISODate
+	);
+	songData.mDuration = results.value(QStringLiteral("DURATION")).toString().toLongLong();
+	songData.mExplicitLyrics = static_cast<ExplicitContent>(
+		results.value(QStringLiteral("EXPLICIT_LYRICS")).toString().toInt()
+	);
+	songData.mPhysicalReleaseDate = QDate::fromString(
+		results.value(QStringLiteral("PHYSICAL_RELEASE_DATE")).toString(),
+		Qt::ISODate
+	);
+	songData.mSngId = results.value(QStringLiteral("SNG_ID")).toString().toLongLong();
+	songData.mSngTitle = results.value(QStringLiteral("SNG_TITLE")).toString();
+
+	songData.mTrackToken = results.value(QStringLiteral("TRACK_TOKEN")).toString();
+	songData.mTrackTokenExpire = QDateTime::fromSecsSinceEpoch(
 		results.value(QStringLiteral("TRACK_TOKEN_EXPIRE")).toInteger()
 	);
 
-	return result;
+	return songData;
+}
+
+auto SongData::Artist::fromJson(const QJsonObject &json) -> Artist
+{
+	Artist artist;
+
+	artist.mArtId = json.value(QStringLiteral("ART_ID")).toString().toLongLong();
+	artist.mArtName = json.value(QStringLiteral("ART_NAME")).toString();
+	artist.mArtPicture = json.value(QStringLiteral("ART_PICTURE")).toString();
+
+	return artist;
 }
 
 auto SongData::trackToken() const -> const QString &
--- src/deezer/objects/songdata.hpp
+++ src/deezer/objects/songdata.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "deezer/enums/explicitcontent.hpp"
+
 #include <QJsonObject>
 
 class SongData final
@@ -7,6 +9,19 @@ class SongData final
 public:
 	[[nodiscard]]
 	static auto fromJson(const QJsonObject &json) -> SongData;
+
+	class Artist final
+	{
+	public:
+		static auto fromJson(const QJsonObject &json) -> Artist;
+
+	private:
+		Artist() = default;
+
+		qint64 mArtId = 0;
+		QString mArtName;
+		QString mArtPicture;
+	};
 
 	[[nodiscard]]
 	auto trackToken() const -> const QString &;
@@ -17,6 +32,16 @@ public:
 private:
 	SongData() = default;
 
+	qint64 mAlbId = 0;
+	QString mAlbPicture;
+	QString mAlbTitle;
+	QList<Artist> mArtists;
+	QDate mDigitalReleaseDate;
+	qint64 mDuration = 0;
+	ExplicitContent mExplicitLyrics = ExplicitContent::NotExplicit;
+	QDate mPhysicalReleaseDate;
+	qint64 mSngId = 0;
+	QString mSngTitle;
 	QString mTrackToken;
 	QDateTime mTrackTokenExpire;
 };
